202546f3e8bbbf82e0d62459d7ca95dab7eeec17
benjamin <benj1276@gmail.com>
2026-04-16 23:50:01 +0200

    fix(web): conditionNames browser to fix import.meta SyntaxError from Supabase/isows

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -19,4 +19,8 @@ config.resolver.nodeModulesPaths = [
 config.resolver.unstable_enableSymlinks = true;
 config.resolver.unstable_enablePackageExports = true;
 
+// Prefer browser-compatible builds to avoid import.meta in non-module scripts
+// (fixes Supabase/isows import.meta SyntaxError on web)
+config.resolver.conditionNames = ['require', 'default', 'browser'];
+
 module.exports = config;
